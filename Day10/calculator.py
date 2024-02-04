1158526352738c4451aa2f21aabedd448080edc8
Pranav Dewangan <pranavdewangan3@gmail.com>
2024-02-04 14:10:56 +0530

    fixed calculator program

--- Day10/calculator.py
+++ Day10/calculator.py
@@ -1,3 +1,5 @@
+operations_list=['+','-','*','/']
+
 def sum(n1,n2):
     return n1+n2
 
@@ -10,33 +12,41 @@ def multiply(n1,n2):
 def divide(n1,n2):
     return n1/n2
 
-operations_list=['+','-','*','/']
+def calculator():
+    end_of_calculation=False
 
-end_of_calculation=False
+    operand1=float(input("What's the first number?: "))
+    result=0
+    while not end_of_calculation:
+        for operator in operations_list:
+            print(operator)
 
-operand1=int(input("What's the first number?: '"))
-result=0
-while not end_of_calculation:
-    for operator in operations_list:
-        print(operator)
-
-    operation=int(input("Pick an operation: "))
-    operand2=int(input("What's the next number?: '"))
-    
-    if operation=='+':
-        result=sum(operand1,operand2)
-    elif operation=='-':
-        result=subtract(operand1,operand2)
+        operation=input("Pick an operation: ")
         
-    elif operation=='*':
-        result=multiply(operand1,operand2)
+        if operation not in operations_list:
+            print("Wrong operation. Program exited.")
+            return
         
-    else:
-        result=divide(operand1,operand2)
+        operand2=float(input("What's the next number?: "))
         
-    operand1=result
-        
-    choice=input(f"Type 'y' to continue calculating with {result}, or type 'n' to start a new calculation: ")
-    if('n'):
-        end_of_calculation=True
+        if operation=='+':
+            result=sum(operand1,operand2)
+        elif operation=='-':
+            result=subtract(operand1,operand2)
+            
+        elif operation=='*':
+            result=multiply(operand1,operand2)
+            
+        elif operation=='/':
+            result=divide(operand1,operand2)
+            
+            
+        operand1=result
+            
+        choice=input(f"Type 'y' to continue calculating with {result}, or type 'n' to start a new calculation: ")
+        if(choice=='n'):
+            end_of_calculation=True
+            calculator()
+            
+calculator()
     
