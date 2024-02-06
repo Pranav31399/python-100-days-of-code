d552633d8fab8ce83bff9cfb2271e3e287a7e783
Pranav Dewangan <pranavdewangan3@gmail.com>
2024-02-06 14:50:05 +0530

    updated blackjack code

--- Day11/blackjack.py
+++ Day11/blackjack.py
@@ -3,13 +3,24 @@ import random
 card_list=[11,2,3,4,5,6,7,8,9,10,10,10,10]
 print("Blackjack program started.")
 
+def draw_card(total_sum):
+    temp=random.choice(card_list)
+    total_sum+=temp
+    if temp==11 and total_sum>21:
+        temp=1
+    return temp
+
 users_card=[]
 computers_card=[]
 
-users_card.append(random.choice(card_list))
-users_card.append(random.choice(card_list))
-computers_card.append(random.choice(card_list))
-computers_card.append(random.choice(card_list))
+users_card_total=0
+computers_card_total=0
+
+for i in range(2):
+    users_card.append(draw_card(users_card_total))
+    users_card_total+=users_card[-1]
+    computers_card.append(draw_card(computers_card_total))
+    computers_card_total+=computers_card[-1]
 
 print(f"users card : {users_card}")
 print(f"computers card : {computers_card}")
@@ -26,16 +37,12 @@ def blackjack():
     print(users_card_total)
     print(computers_card_total)
 
-    if users_card_total==21 or computers_card_total==21:
-        if users_card_total==21 and computers_card_total==21 :
-            print("Draw.")
-            return
-        elif users_card_total==21 :
-            print("You win.")
-            return
-        elif computers_card_total==21 :
-            print("You lose.")
-            return
+    if computers_card_total==21 :
+        print("You lose.")
+        return
+    elif users_card_total==21 :
+        print("You win.")
+        return
 
     else:
         if users_card_total > 21:
@@ -44,12 +51,13 @@ def blackjack():
         else:
             another_card=input("Do you want to get annother card : ")
             if(another_card=='yes'):
-                users_card.append(random.choice(card_list))
+                users_card.append(draw_card(users_card_total))
+                users_card_total+=users_card[-1]
                 print(f"users card : {users_card}")
                 blackjack()
             else:
                 while(computers_card_total<17):
-                    computers_card.append(random.choice(card_list))
+                    computers_card.append(draw_card(computers_card_total))
                     print(f"users card : {users_card}")
                     print(f"computers card : {computers_card}")
                     computers_card_total+=computers_card[-1]
@@ -64,3 +72,4 @@ def blackjack():
                     print("Draw.")
                     
 blackjack()
+
